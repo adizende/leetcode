ff3cd00b3701e9d407f2fc94803fe6bcb2f5b08a
Aditya Zende <adizende8003@gmail.com>
2020-08-12 23:01:03 -0700

    adding new file

---
+++ Move zeros.py
@@ -0,0 +1,13 @@
+def movezeros(nums):
+    i = 0
+    for j in range(len(nums)):
+        if nums[j] != 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+    return nums
+print(movezeros([0,1,0,3,12]))
+
+# two pointer i and j
+# I will increase i if and only if swaping nums[j] and nums[i] and will only swap when nums[j] not zero
+# i=0 and j = 0      j=1 i=0 (swap)  j=1 i=1 [1,0,0,3,12]  j=2 i=1  j=3 i=2[1,3,0,0,12]  j=4 i=3[1,3,12,0,0]
+
